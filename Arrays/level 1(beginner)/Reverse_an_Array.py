59617478f48aae1f345052296863a950ccb19084
Lohith Reddy Bodumallu <lohithreddyb@gmail.com>
2025-11-13 12:52:09 +0530

    Create Reverse_an_Array.py

---
+++ Arrays/level 1(beginner)/Reverse_an_Array.py
@@ -0,0 +1,19 @@
+# ------------------------------
+# Problem: Reverse an Array
+# Reverse the order of elements in a given array.
+#
+# Example:
+# Input:  [1, 2, 3, 4, 5]
+# Output: [5, 4, 3, 2, 1]
+# ------------------------------
+# Author : Lohith Reddy Bodumallu
+# Language : Python
+# Approach : Using slicing (O(n))
+# ------------------------------
+
+def reverse_array(arr):
+    return arr[::-1]
+
+# Example Run
+arr = [1, 2, 3, 4, 5]
+print("Reversed Array:", reverse_array(arr))
